03549932405b67080f4c62fdcfc3490935f2b2b9
Yasin Köse <yasin.kose.123@gmail.com>
2022-02-03 23:24:17 +0300

    Update language file

--- resources/lang/en/lang.php
+++ resources/lang/en/lang.php
@@ -8,5 +8,14 @@ return [
         'email' => 'Mail Adresi',
         'password' => 'Şifre',
         'response' => 'Giriş başarısız!',
+        'no-matching-account-found' => 'Eşleşen hesap bulunamadı!',
+        'successful' => 'Giriş başarılı!',
+    ],
+    'register' => [
+        'response' => 'Kayıt başarısız!',
+        'name' => 'Ad',
+        'surname' => 'Soyad',
+        'email' => 'Mail adresi',
+        'password' => 'Şifre',
     ],
 ];
