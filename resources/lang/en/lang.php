59552107b98b0fd09539743cd1d497ea773cffec
Yasin Köse <yasin.kose.123@gmail.com>
2022-02-04 00:34:33 +0300

    Update language file

--- resources/lang/en/lang.php
+++ resources/lang/en/lang.php
@@ -5,10 +5,11 @@ return [
         'form-request' => 'Validasyon işleminiz başarısız',
     ],
     'validation' => [
-        'email' => 'Mail Adresi',
-        'password' => 'Şifre',
         'name' => 'Ad',
         'surname' => 'Soyad',
+        'email' => 'Mail Adresi',
+        'password' => 'Şifre',
+        'token' => "Doğrulama kodu"
     ],
     'login' => [
         'response-message' => 'Giriş başarısız!',
@@ -20,5 +21,24 @@ return [
     ],
     'reset-password' => [
         'response-message' => 'Şifre sıfırlama isteği gönderimi başarısız!',
+        'user-not-found' => 'Eşleşen bir hesap bulunamadı!',
+        'successful' => 'Parola sıfırlama isteğiniz başarıyla gönderilmiştir, lütfen gelen kutunuzu kontrol edin',
+    ],
+    'mail' => [
+        'forgot-password' => [
+            'subject' => 'Parolamı Unuttum',
+        ],
+    ],
+    'reset-password-token' => [
+        'response-message' => 'Şifre sıfırlama işlemi başarısız!',
+        'expired' => 'Tokenin süresi dolmuştur',
+        'successful' => 'Parolanız başarıyla sıfırlanmıştır.',
+        'invalid-token' => 'Geçersiz token girildi!'
+    ],
+    'check-reset-password-token' => [
+        'response-message' => 'Geçersiz token!',
+        'expired' => 'Tokenin süresi dolmuştur',
+        'invalid-token' => 'Geçersiz token girildi!',
+        'successful' => 'Token kullanılabilir!'
     ],
 ];
