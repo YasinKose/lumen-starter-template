7e6515db4235e3d266b72a48600be0a6ca86fff6
Yasin Köse <yasin.kose.123@gmail.com>
2022-02-03 23:51:02 +0300

    Create resetPassword method in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\ResetPasswordRequest;
 use App\Models\User;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 use Respond;
 
 class AuthController extends Controller
@@ -33,5 +38,32 @@ class AuthController extends Controller
     public function register(RegisterRequest $request)
     {
         return Respond::ok("OK", User::create($request->validated()));
+    }
+
+    public function resetPassword(ResetPasswordRequest $request)
+    {
+        $user = User::select(['id', 'email'])
+            ->where('email', $request->input("email"))
+            ->first();
+
+        if (!$user) {
+            return Respond::notFound(__('lang.reset-password.user-not-found'));
+        }
+
+        $token = Str::random(32);
+
+        DB::table('password_resets')->insert([
+            'email' => $user->email,
+            'user_id' => $user->id,
+            'token' => $token,
+            'created_at' => Carbon::now()
+        ]);
+
+        Mail::send('email.forgot-password', ['token' => $token, 'user' => $user], function ($message) use ($user) {
+            $message->to($user->email);
+            $message->subject(__('lang.mail.forgot-password.subject'));
+        });
+
+        return Respond::ok(__('lang.reset-password.successful'));
     }
 }
