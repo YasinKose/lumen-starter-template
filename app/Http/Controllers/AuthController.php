41b914462994cdc585039621d8f40110d9b2eae8
Yasin Köse <yasin.kose.123@gmail.com>
2022-02-03 23:27:57 +0300

    Create register method in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Respond;
 
 class AuthController extends Controller
 {
+    /**
+     * @param LoginRequest $request
+     * @return mixed
+     */
     public function login(LoginRequest $request)
     {
         if (!$token = auth()->attempt($request->validated())) {
@@ -18,5 +24,14 @@ class AuthController extends Controller
             'token_type' => 'Bearer',
             'expires_in' => auth()->factory()->getTTL() * 60
         ]);
+    }
+
+    /**
+     * @param RegisterRequest $request
+     * @return mixed
+     */
+    public function register(RegisterRequest $request)
+    {
+        return Respond::ok("OK", User::create($request->validated()));
     }
 }
