061dcbfaf5b01149310554f16d9016e355c02eed
Yasin Köse <yasin.kose.123@gmail.com>
2022-02-03 21:53:12 +0300

    Create language file

---
+++ resources/lang/en/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'default' => [
+        'form-request' => 'Validasyon işleminiz başarısız',
+    ],
+    'login' => [
+        'email' => 'Mail Adresi',
+        'password' => 'Şifre',
+        'response' => 'Giriş başarısız!',
+    ],
+];
