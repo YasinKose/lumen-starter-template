314f957fa7ac237d2ced1301bbb00ff7fa2322df
Yasin Köse <yasin.kose.123@gmail.com>
2022-02-07 23:15:31 +0300

    Update fillable attributes in User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'email',
+        'name', 'surname', 'email', 'password'
     ];
 
     /**
